ea798a94d7ebafccffe17b5f6bcb4f6c88da64d7
Ma7Moud-Elsakkar <mahmoudelsakkar12@gmail.com>
2025-08-16 18:36:46 +0300

    5

--- app/components/cell.tsx
+++ app/components/cell.tsx
@@ -1,46 +1,53 @@
-import React from 'react';
+import { Dispatch } from "react";
+
 
 type CellProps = {
     id: number;
     go: string;
-    setGo: React.Dispatch<React.SetStateAction<string>>;
+    setGo: Dispatch<React.SetStateAction<string>>;
     cells: string[];
-    setCells: React.Dispatch<React.SetStateAction<string[]>>;
+    setCells: Dispatch<React.SetStateAction<string[]>>;
     cell: string;
-    winningMessage: string;
-};
+    winningMatch: string;
+}
 
-const Cell = ({ id, go, setGo, cells, setCells, cell, winningMessage }: CellProps) => {
+
+const Cell = ({ id, go, setGo, cells, setCells, cell, winningMatch }: CellProps) => {
+
     const handleClick = () => {
-        if (winningMessage) {
+
+        if (winningMatch) {
             return;
         }
         
         const notTaken = !cells[id];
         if (notTaken) {
-            if (go === "circle") {
-                handleCellChange("circle");
+
+        
+            if(go === "circle" ) {
+                cellChange("circle")
                 setGo("cross");
-            } else if (go === "cross") {
-                handleCellChange("cross");
+            } else if(go === "cross") {
+                cellChange("cross")
                 setGo("circle");
             }
         }
-    };
+    }
 
-    const handleCellChange = (cellValue: string) => {
+    const cellChange =  (celltoChange: string) => {
         let newCells = [...cells];
-        newCells[id] = cellValue;
+        newCells[id] = celltoChange;
         setCells(newCells);
-    };
-
+    }
     return (
         <div className="square" onClick={handleClick}>
             <div className={cell}>
-                {cell ? (cell === "circle" ? "O" : "X") : ""}
+                {cell ? (cell === "circle" ? "O" : "X" ) : ""}
             </div>
         </div>
     );
-};
+}
+
+
 
 export default Cell;
